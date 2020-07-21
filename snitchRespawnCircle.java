80af1be614c0b4c25c68e1945fd563819ac77b23
MadBeignet <67487326+MadBeignet@users.noreply.github.com>
2020-07-21 06:13:55 -0500

    Update snitchRespawnCircle.java

--- snitchRespawnCircle.java
+++ snitchRespawnCircle.java
@@ -1,13 +1,17 @@
-public static void main(String[] args) {
+public class respawnSnitch {
+	public static void main(String[] args) {
 		int seekerHx, seekerHz,seekerAx,seekerAz,range;
-		seekerHx = -313;
-		seekerHz = -293;
-		seekerAx = -313;
-		seekerAz = -293;
+		seekerHx = 278;
+		seekerHz = 196;
+		seekerAx = 25;
+		seekerAz = 96;
 		range = 100;
 	
 		int[] snitchCoord = snitchSpawn(seekerHx,seekerHz,seekerAx,seekerAz,range);
-		
+		while(snitchCoord[1]<=298 && snitchCoord[1]>=-293&&Math.pow(snitchCoord[0]-seekerHx,2)+Math.pow(snitchCoord[1]-seekerHz, 2)>=Math.pow(100, 2) && Math.pow(snitchCoord[0]-seekerAx,2)+Math.pow(snitchCoord[1]-seekerAz, 2)>=Math.pow(100, 2)) {
+		System.out.println("x: "+ snitchCoord[0]+"\nz: "+ snitchCoord[1]);
+		snitchCoord = snitchSpawn(seekerHx,seekerHz,seekerAx,seekerAz,range);
+		}
 		System.out.println("x: "+ snitchCoord[0]+"\nz: "+ snitchCoord[1]);
 	}
 public static int[] snitchSpawn(int seekerHx, int seekerHz, int seekerAx, int seekerAz, int range) {
@@ -19,39 +23,74 @@ public static int[] snitchSpawn(int seekerHx, int seekerHz, int seekerAx, int se
 		int z = circle(x, seekerHz, seekerHx, seekerAz, seekerAx, range, quidMinz, quidMaxz);
 		return new int[] {x,z};
 	}
-  public static int circle(int x,int centerhz,int centerhx, int centeraz, int centerax, int range, int quidMinz, int quidMaxz) {
-		int posCirclehz = (int)(Math.sqrt(Math.abs(Math.pow(range, 2)-Math.pow(x-centerhx, 2))))+centerhz+1;
-		int negCirclehz = (int)((-(Math.sqrt(Math.abs(Math.pow(range, 2)-Math.pow(x-centerhx, 2))))))+centerhz+1;
-		if(posCirclehz >quidMaxz)
-			posCirclehz = quidMaxz;
-		if(negCirclehz<quidMinz)
-			negCirclehz = quidMinz;
-		int posCircleaz = (int)(Math.sqrt(Math.abs(Math.pow(range, 2)-Math.pow(x-centerax, 2))))+centeraz+1;
-		int negCircleaz = (int)((-(Math.sqrt(Math.abs(Math.pow(range, 2)-Math.pow(x-centerax, 2)))))+centeraz+1);
-		if(posCircleaz >quidMaxz)
-			posCircleaz = quidMaxz;
-		if(negCircleaz<quidMinz)
-			negCircleaz = quidMinz;
+ public static int circle(int x,int centerhz,int centerhx, int centeraz, int centerax, int range, int quidMinz, int quidMaxz) {
+	  int posCirclehz = -500, negCirclehz = -500, posCircleaz =-500, negCircleaz= -500;
+	  
+	  
+	  if(Math.pow(range, 2)-Math.pow(x-centerhx, 2)>=0){	
+	   posCirclehz = (int)((Math.sqrt(Math.pow(range, 2)-Math.pow(x-centerhx, 2)))+centerhz)+1;
+		 negCirclehz = (int)(((-(Math.sqrt(Math.pow(range, 2)-Math.pow(x-centerhx, 2)))))+centerhz)-1;
+		 if(posCirclehz >quidMaxz)
+				posCirclehz = quidMaxz;
+			if(negCirclehz<quidMinz)
+				negCirclehz = quidMinz;
+	}
+	if((Math.pow(range, 2)-Math.pow(x-centerax, 2))>=0)	{
+		 posCircleaz = (int)((Math.sqrt(Math.abs(Math.pow(range, 2)-Math.pow(x-centerax, 2))))+centeraz)+1;
+		 negCircleaz = (int)((-(Math.sqrt(Math.abs(Math.pow(range, 2)-Math.pow(x-centerax, 2)))))+centeraz)-1;
+		 if(posCircleaz >quidMaxz)
+				posCircleaz = quidMaxz;
+			if(negCircleaz<quidMinz)
+				negCircleaz = quidMinz;
+	}
 		int rangez1 = 0;
 		int rangez2 = 0;
-		if(posCirclehz>=negCircleaz) {
-			rangez1 = Math.abs(posCirclehz-negCirclehz) + Math.abs(posCircleaz-negCircleaz)-(Math.abs(posCirclehz-negCircleaz));
-			rangez2 = 0;
+		if(posCirclehz>negCircleaz&&negCircleaz!=-500) {
+			rangez1 = Math.abs(posCirclehz-negCirclehz) -(Math.abs(posCirclehz-negCircleaz));
+			rangez2 = Math.abs(posCircleaz-negCircleaz);
 		}
-		if(posCircleaz>=negCirclehz) {
-			rangez1 = Math.abs(posCirclehz-negCirclehz)+Math.abs(posCircleaz-negCircleaz)-(Math.abs(posCircleaz-negCirclehz));
-			rangez2 = 0;
+		if(posCircleaz>negCirclehz&&negCirclehz!=-500) {
+			rangez1 = Math.abs(posCirclehz-negCirclehz)-(Math.abs(posCircleaz-negCirclehz));
+			rangez2 = Math.abs(posCircleaz-negCircleaz);
 		}
-		if(posCirclehz<=negCircleaz||posCircleaz<=negCirclehz) {
+		if(posCirclehz<negCircleaz||posCircleaz<=negCirclehz&&negCircleaz!=-500&&negCirclehz!=-500) {
 		rangez1= Math.abs(negCirclehz-posCirclehz);
 		rangez2= Math.abs(negCircleaz-posCircleaz);
 		}
-		int spawnz = (int)(Math.random()*(quidMaxz-quidMinz-rangez1-rangez2)+quidMinz);
-		if(spawnz < posCirclehz&&spawnz>negCirclehz||spawnz<posCircleaz&&spawnz>negCircleaz) {
+		if(Math.pow(range, 2)-Math.pow(x-centerhx, 2)<0&&(Math.pow(range, 2)-Math.pow(x-centerax, 2))>=0){
+		
+		rangez2= Math.abs(negCircleaz-posCircleaz);
+		}
+		if((Math.pow(range, 2)-Math.pow(x-centerax, 2))<0&&Math.pow(range, 2)-Math.pow(x-centerhx, 2)>=0) {
+			
+			rangez1=Math.abs(negCirclehz-posCirclehz);
+		}
+
+		System.out.println(rangez1);
+		System.out.println(rangez2);
+		int spawnz = (int)(Math.random()*(quidMaxz-quidMinz-rangez1-rangez2)+quidMinz+1);
+
+		if((rangez1!=0&&rangez2!=0&&rangez1>0)&&spawnz <= posCirclehz&&spawnz>=negCirclehz) {
 			spawnz+=rangez1;
+			if(spawnz<=posCircleaz&&spawnz>=negCircleaz)
+				spawnz+=rangez2;
 		}
-		if(spawnz <posCircleaz&&spawnz>negCircleaz) {
+		System.out.println(spawnz);
+		if((rangez1!=0&&rangez2!=0&&rangez1>0)&&spawnz<=posCircleaz&&spawnz>=negCircleaz) {
 			spawnz+=rangez2;
+			if(spawnz<=posCirclehz&&spawnz>=negCirclehz)
+				spawnz+=rangez1;
 		}
+		if(rangez1<0&&(spawnz<=posCirclehz&&spawnz>=negCirclehz||spawnz<=posCircleaz&&spawnz>=negCircleaz)) {
+			spawnz+=rangez1+rangez2;
+		}
+		System.out.println(spawnz);
+		if(rangez1==0&&rangez2!=0&&spawnz<=posCircleaz&&spawnz>=negCircleaz)
+			spawnz+=rangez2;
+		System.out.println(spawnz);
+		if(rangez2==0&&rangez1!=0&&spawnz<=posCirclehz&&spawnz>=negCirclehz)
+			spawnz+=rangez1;
+		System.out.println(spawnz);
 		return spawnz;
+}
 }
