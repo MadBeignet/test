7bad211869b0e0b3209957acf54a948fce8a98de
MadBeignet <67487326+MadBeignet@users.noreply.github.com>
2020-07-22 00:15:36 -0500

    Update respawnSnitchCoords.java

--- respawnSnitchCoords.java
+++ respawnSnitchCoords.java
@@ -14,7 +14,7 @@ public class respawnSnitch {
 		//}
 		//System.out.println("x: "+ snitchCoord[0]+"\nz: "+ snitchCoord[1]);
 	}
-public static int[] snitchSpawn(int seekerHx, int seekerHz, int seekerAx, int seekerAz, int range) {
+  public static int[] snitchSpawn(int seekerHx, int seekerHz, int seekerAx, int seekerAz, int range) {
 		int quidMinx = -313;
 		int quidMaxx = 278;
 		int quidMinz = -293;
@@ -45,90 +45,83 @@ public static int[] snitchSpawn(int seekerHx, int seekerHz, int seekerAx, int se
 		 if(posCircleaz >quidMaxz)
 				posCircleaz = quidMaxz; // again, can't be a point outside of the map
 			if(negCircleaz<quidMinz)
-				negCircleaz = quidMinz;
-		
-			
+				negCircleaz = quidMinz;		
 	}
-		
+	int rangez1 = 0;
+	int rangez2 = 0;
+	// if away circle doesn't exist but home does this created a problem so the -500 was needed
+	  // testing for overlap between the circles
+	if(posCirclehz>negCircleaz&&negCircleaz!=-500&&Math.abs(posCirclehz-negCircleaz)<Math.abs(posCirclehz-negCirclehz)) {
+		rangez1 = (int)(Math.abs(posCirclehz-negCirclehz) -(Math.abs(posCirclehz-negCircleaz)))+1;
+		rangez2 = (int) Math.abs(posCircleaz-negCircleaz)+1;
 	
-		int rangez1 = 0;
-		int rangez2 = 0;
-		// if away circle doesn't exist but home does this created a problem so the -500 was needed
-	  	// testing for overlap between the circles
-		if(posCirclehz>negCircleaz&&negCircleaz!=-500&&Math.abs(posCirclehz-negCircleaz)<Math.abs(posCirclehz-negCirclehz)) {
-			rangez1 = (int)(Math.abs(posCirclehz-negCirclehz) -(Math.abs(posCirclehz-negCircleaz)))+1;
-			rangez2 = (int) Math.abs(posCircleaz-negCircleaz)+1;
+	}
+	// same as above
+	if(posCircleaz>negCirclehz&&negCirclehz!=-500&&Math.abs(posCircleaz-negCirclehz)<Math.abs(posCircleaz-negCircleaz)) {
+		rangez1 = (int)(Math.abs(posCirclehz-negCirclehz)-(Math.abs(posCircleaz-negCirclehz)))+1;
+		rangez2 = (int) Math.abs(posCircleaz-negCircleaz)+1;
 	
-		}
-		// same as above
-		if(posCircleaz>negCirclehz&&negCirclehz!=-500&&Math.abs(posCircleaz-negCirclehz)<Math.abs(posCircleaz-negCircleaz)) {
-			rangez1 = (int)(Math.abs(posCirclehz-negCirclehz)-(Math.abs(posCircleaz-negCirclehz)))+1;
-			rangez2 = (int) Math.abs(posCircleaz-negCircleaz)+1;
-	
-		}
-		// checks if one range is inside of the other range and accounts for that 
-		if((negCirclehz!=-500&&negCircleaz!=-500)&&((posCirclehz>=posCircleaz&&negCirclehz<=negCircleaz)||(posCircleaz>=posCirclehz&&negCircleaz<=negCirclehz))) {
-			if(Math.abs(posCirclehz-negCirclehz)>Math.abs(posCircleaz-negCircleaz)) {
-				rangez1=(int) Math.abs(posCirclehz-negCirclehz)/2+1;
-				rangez2=(int) Math.abs(posCirclehz-negCirclehz)/2;
-			}else {
+	}
+	// checks if one range is inside of the other range and accounts for that 
+	if((negCirclehz!=-500&&negCircleaz!=-500)&&((posCirclehz>=posCircleaz&&negCirclehz<=negCircleaz)||(posCircleaz>=posCirclehz&&negCircleaz<=negCirclehz))) {
+		if(Math.abs(posCirclehz-negCirclehz)>Math.abs(posCircleaz-negCircleaz)) {
+			rangez1=(int) Math.abs(posCirclehz-negCirclehz)/2+1;
+			rangez2=(int) Math.abs(posCirclehz-negCirclehz)/2;
+		}else {
 				
 			rangez1=(int)Math.abs(posCircleaz-negCircleaz)/2;
 			rangez2=(int)Math.abs(posCircleaz-negCircleaz)/2;
-			}
-		} // this one basically just makes sure there is no overlap and that the bigger value on one circle is less than the smaller value on another
-		if((negCircleaz!=-500&&negCirclehz!=-500)&&(posCirclehz<negCircleaz||posCircleaz<=negCirclehz)) {
+		}
+	} // this one basically just makes sure there is no overlap and that the bigger value on one circle is less than the smaller value on another
+	if((negCircleaz!=-500&&negCirclehz!=-500)&&(posCirclehz<negCircleaz||posCircleaz<=negCirclehz)) {
 		
 		rangez1= (int) Math.abs(negCirclehz-posCirclehz)+1;
 		rangez2= (int) Math.abs(negCircleaz-posCircleaz)+1;
-		}
+	}
 		// this one is for if home circle doesn't exist, rangez1 = 0 in this case
-		if(Math.pow(range, 2)-Math.pow(x-centerhx, 2)<0&&(Math.pow(range, 2)-Math.pow(x-centerax, 2))>=0){
+	if(Math.pow(range, 2)-Math.pow(x-centerhx, 2)<0&&(Math.pow(range, 2)-Math.pow(x-centerax, 2))>=0){
 		
 		rangez2= (int) Math.abs(negCircleaz-posCircleaz)+1;
-		}
+	}
 		// this one is for if away circle doesn't exist, rangez2 = 0 in this case
-		if((Math.pow(range, 2)-Math.pow(x-centerax, 2))<0&&Math.pow(range, 2)-Math.pow(x-centerhx, 2)>=0) {
+	if((Math.pow(range, 2)-Math.pow(x-centerax, 2))<0&&Math.pow(range, 2)-Math.pow(x-centerhx, 2)>=0) {
 			
-			rangez1=(int)Math.abs(negCirclehz-posCirclehz)+1;
-		}
+		rangez1=(int)Math.abs(negCirclehz-posCirclehz)+1;
+	}
 		// this basically calculates the number of spawnable places for the snitch in the one line of the random x value entered from main method
-		int spawnz = (int)(Math.random()*(quidMaxz-quidMinz-rangez1-rangez2)+quidMinz);
+	int spawnz = (int)(Math.random()*(quidMaxz-quidMinz-rangez1-rangez2)+quidMinz);
 		
 		//this is basically for points where the one range is around the other, but the point (spawnz) is inside one or both of the ranges
-		if(((posCirclehz>=posCircleaz&&negCirclehz<=negCircleaz)||(posCircleaz>=posCirclehz&&negCircleaz<=negCirclehz))&&((spawnz<posCirclehz&&spawnz>negCirclehz)||(spawnz<posCircleaz&&spawnz>negCircleaz))){
+	if(((posCirclehz>=posCircleaz&&negCirclehz<=negCircleaz)||(posCircleaz>=posCirclehz&&negCircleaz<=negCirclehz))&&((spawnz<posCirclehz&&spawnz>negCirclehz)||(spawnz<posCircleaz&&spawnz>negCircleaz))){
 			spawnz+=rangez1+rangez2;
-		}
+	}
 		// makes sure both circles exist in the line and then sees if the point picked is in the unspawnable range, if so, adds the range of the circle on that line
-		if((rangez1!=0&&rangez2!=0&&rangez1>0)&&spawnz <= posCirclehz&&spawnz>=negCirclehz) {
-			spawnz+=rangez1; //adds the range of the other circle if it runs into it after being added above
+	if((rangez1!=0&&rangez2!=0&&rangez1>0)&&spawnz <= posCirclehz&&spawnz>=negCirclehz) {
+		spawnz+=rangez1; //adds the range of the other circle if it runs into it after being added above
 			
-			if(spawnz<=posCircleaz&&spawnz>=negCircleaz) {
+		if(spawnz<=posCircleaz&&spawnz>=negCircleaz) {
 				spawnz+=rangez2;
-			}		
-		}// same thing as above just flip flopped
-		if((rangez1!=0&&rangez2!=0&&rangez1>0)&&spawnz<=posCircleaz&&spawnz>=negCircleaz) {
-			spawnz+=rangez2;
+		}		
+	}// same thing as above just flip flopped
+	if((rangez1!=0&&rangez2!=0&&rangez1>0)&&spawnz<=posCircleaz&&spawnz>=negCircleaz) {
+		spawnz+=rangez2;
 
-			if(spawnz<=posCirclehz&&spawnz>=negCirclehz) {
+		if(spawnz<=posCirclehz&&spawnz>=negCirclehz) {
 				spawnz+=rangez1;
-			}
-			if((rangez1!=0&&rangez2!=0&&rangez1>0)&&spawnz<=posCirclehz&&spawnz<=posCircleaz&&spawnz>=negCirclehz&&spawnz>=negCircleaz){
-				spawnz+=rangez1+rangez2;		
-			}
-		}// because I separated rangez1 and rangez2 when the circles overlapped, one was negative, so I made another statement because it would jump 160 blocks and not go back
-		if(rangez1<0&&(spawnz<=posCirclehz&&spawnz>=negCirclehz||spawnz<=posCircleaz&&spawnz>=negCircleaz)) {
-			spawnz+=rangez1+rangez2;
 		}
+	}// because I separated rangez1 and rangez2 when the circles overlapped, one was negative, so I made another statement because it would jump 160 blocks and not go back
+	if(rangez1<0&&(spawnz<=posCirclehz&&spawnz>=negCirclehz||spawnz<=posCircleaz&&spawnz>=negCircleaz)) {
+		spawnz+=rangez1+rangez2;
+	}
 		// here we reach the tests for nonexistent circles and if the z value needs to be shifted by the range of the existent circle
-		if(rangez1==0&&rangez2!=0&&spawnz<=posCircleaz&&spawnz>=negCircleaz) {
-			spawnz+=rangez2;	
-		}
-		if(rangez2==0&&rangez1!=0&&spawnz<=posCirclehz&&spawnz>=negCirclehz) {
-			spawnz+=rangez1;
-		}
+	if(rangez1==0&&rangez2!=0&&spawnz<=posCircleaz&&spawnz>=negCircleaz) {
+		spawnz+=rangez2;	
+	}
+	if(rangez2==0&&rangez1!=0&&spawnz<=posCirclehz&&spawnz>=negCirclehz) {
+		spawnz+=rangez1;
+	}
 		// obviously no need to test if the ranges are both 0 because whatever random z value picked from the original spawnz equation would work
 		
-		return spawnz; //returns the value for the z coordinate that the snitch should spawn at
-  	}
+	return spawnz; //returns the value for the z coordinate that the snitch should spawn at
+   }
 }
